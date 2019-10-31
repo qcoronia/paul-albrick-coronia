677961c11924be3a4faef3037991dcbc3d440856
Paul Albrick Coronia <qcoronia@gmail.com>
2019-10-31 23:55:13 +0800

    Update styles on landing page sections

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFontAwesomeModule
   ],
   exports: [
     NavbarComponent,
