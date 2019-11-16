a42a0785057f33887f5ff0c64b135204c1007677
Paul Albrick Coronia <qcoronia@gmail.com>
2019-11-16 21:07:34 +0800

    Add banner component

---
+++ src/app/home/sections/sections.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
+
+
+
+@NgModule({
+  declarations: [BannerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SectionsModule { }
