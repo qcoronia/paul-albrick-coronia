76b0ebad6638c117ba5a3eeae4619dba31a92d1f
Paul Albrick Coronia <qcoronia@gmail.com>
2019-11-24 21:01:57 +0800

    Fix build error on environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  socialMedia: {
+    twitter: {
+      url: 'https://twitter.com/nYTskYz',
+      label: '@nYTskYz',
+    },
+    email: {
+      url: 'mailto:qcoronia@gmail.com',
+      label: 'qcoronia@gmail.com',
+    },
+  },
 };
